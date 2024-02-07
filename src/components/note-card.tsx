9241fd9a1568e35d348ff59ccc62741160d3f003
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2024-02-07 12:54:51 -0300

    feat: create note card component

---
+++ src/components/note-card.tsx
@@ -0,0 +1,17 @@
+export function NoteCard() {
+  return (
+    <button className="text-left rounded-md bg-zinc-800 p-5 space-y-3 outline-none hover:ring-2 hover:ring-zinc-600 focus-visible:ring-2 focus-visible:ring-pink-400 overflow-hidden">
+      <span className="text-sm font-medium text-zinc-300">há 2 dias</span>
+      <p className="text-sm leading-6 text-zinc-400">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
+        tempore mollitia modi? Praesentium, suscipit deserunt omnis, dolor
+        tenetur mollitia, consequatur sunt sint nobis accusamus odio facere
+        architecto asperiores eveniet nihil! Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Architecto tempore mollitia modi?
+        Praesentium, suscipit deserunt omnis, dolor tenetur mollitia,
+        consequatur sunt sint nobis accusamus odio facere architecto asperiores
+        eveniet nihil!
+      </p>
+    </button>
+  );
+}
